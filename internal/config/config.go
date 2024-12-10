c718e50aa4da4cdf439bed1b7630a82742b7ff5f
EugeneTsydenov <ecydenov110@gmail.com>
2024-12-10 22:14:33 +0800

    feat: add config package(Config and DatabaseConfig) using go envconfig tag

--- internal/config/config.go
+++ internal/config/config.go
@@ -5,22 +5,22 @@ import (
 	"github.com/sethvargo/go-envconfig"
 )
 
-type DatabaseConfig struct {
-	DatabaseUrl string `env:"DATABASE_URL, required"`
+type Config struct {
+	Database DatabaseConfig
 }
 
-type Config struct {
-	Database *DatabaseConfig
+type DatabaseConfig struct {
+	DatabaseUrl string `env:"DATABASE_URL, required"`
 }
 
 func Load() (*Config, error) {
 	ctx := context.Background()
 
-	var c *Config
+	var c Config
 
 	if err := envconfig.Process(ctx, &c); err != nil {
 		return nil, err
 	}
 
-	return c, nil
+	return &c, nil
 }
