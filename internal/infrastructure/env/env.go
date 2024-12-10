14495727a883474528afeb41c09565950194ad66
EugeneTsydenov <ecydenov110@gmail.com>
2024-12-10 19:16:11 +0800

    chore

--- internal/infrastructure/env/env.go
+++ internal/infrastructure/env/env.go
@@ -3,55 +3,51 @@ package env
 import (
 	"fmt"
 	"github.com/joho/godotenv"
-	"log"
 	"os"
 )
 
 const (
-	nodeEnvKey     = "NODE_ENV"
-	defaultNodeEnv = "production"
+	nodeEnvKey         = "NODE_ENV"
+	localNodeEnv       = "local"
+	developmentNodeEnv = "development"
+	productionNodeEnv  = "production"
 )
 
-func Load() {
+var allowedNodeEnvs = map[string]string{
+	localNodeEnv:       "",
+	developmentNodeEnv: "",
+	productionNodeEnv:  "",
+}
+
+func Load() error {
 	nodeEnv := getNodeEnv()
 	envFileName := getEnvFileName(nodeEnv)
 
 	if err := godotenv.Load(envFileName); err != nil {
-		log.Printf("Environment file '%s' not found, continuing with default environment variables.", envFileName)
+		return fmt.Errorf("environment file '%s' not found, continuing with default environment variables: %w", envFileName, err)
 	}
-}
 
-func Get(key string) string {
-	return os.Getenv(key)
-}
-
-func Set(key, value string) error {
-	err := os.Setenv(key, value)
-	if err != nil {
-		return fmt.Errorf("error setting environment variable %s: %w", key, err)
-	}
 	return nil
 }
 
 func getNodeEnv() string {
 	nodeEnv := os.Getenv(nodeEnvKey)
-
-	if nodeEnv == "" {
-		setDefaultNodeEnv()
-		nodeEnv = defaultNodeEnv
+	if nodeEnv == "" || !validateNodeEnv(nodeEnv) {
+		return getDefaultNodeEnv()
 	}
 
 	return nodeEnv
 }
 
-func setDefaultNodeEnv() {
-	if err := os.Setenv(nodeEnvKey, defaultNodeEnv); err != nil {
-		log.Fatalf("Error setting NODE_ENV to %s: %v", defaultNodeEnv, err)
-	}
+func getDefaultNodeEnv() string {
+	return productionNodeEnv
+}
 
-	log.Printf("%s set to %s successfully.", nodeEnvKey, defaultNodeEnv)
+func validateNodeEnv(nodeEnv string) bool {
+	_, ok := allowedNodeEnvs[nodeEnv]
+	return ok
 }
 
 func getEnvFileName(nodeEnv string) string {
-	return fmt.Sprintf(".%s.env", nodeEnv)
+	return fmt.Sprintf("C:/Important/For-work/Programming/Backend-development/current-project/chesshub/services/users-service/.env.%s", nodeEnv)
 }
